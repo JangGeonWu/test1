306470efd1b7a84c92371fc66aba77cf401849a0
GeonWu Jang <today4656fl@naver.com>
2022-06-23 18:01:59 +0900

    first commit

---
+++ binary_search.py
@@ -0,0 +1,60 @@
+
+# n = int(input())
+
+# array_n = list(map(int, input().split()))
+
+# array_n.sort()
+
+# m = int(input())
+
+# array_m = list(map(int, input().split()))
+
+# def binary_search(array, target, start, end):
+#     if start > end:
+#         return None
+#     mid = (start + end) // 2
+
+#     if array[mid] == target:
+#         return mid
+#     elif array[mid] > target:
+#         return binary_search(array, target, start, mid-1)
+#     else:
+#         return binary_search(array, target, mid+1, end)
+
+
+# for i in range (0,m):
+#     if binary_search(array_n, array_m[i], 0, n-1) == None:
+#         print("no", end = ' ')
+#     else: print("yes", end = ' ')
+
+n, m = map(int, input().split())
+
+len_array = list(map(int, input().split()))
+
+len_array.sort()
+
+max_len = max(len_array)
+
+
+while(True):
+    tot = 0
+
+    for i in range(0, n):
+        if len_array[i] - max_len >= 0:
+            tot += len_array[i] - max_len
+
+    if tot >= m:
+        break
+    else:
+        max_len -= 1
+
+print(max_len)
+
+
+
+
+
+
+
+
+
